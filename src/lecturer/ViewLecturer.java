66f6091376e26cbedf173e182365fc322bf06ba5
hasitha <hasithakeshana9900@gmail.com>
2020-09-04 16:51:46 +0530

    fixed mini issue

--- src/lecturer/ViewLecturer.java
+++ src/lecturer/ViewLecturer.java
@@ -47,6 +47,9 @@ public class ViewLecturer extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
+		
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
